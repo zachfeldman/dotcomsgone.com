638d6bfddbffa7b15bd2d3688a45450435e88cc7
Zach Feldman <zachfeldman@gmail.com>
2014-12-25 16:46:21 -0500

    Cleanup.

--- scrape.rb
+++ scrape.rb
@@ -4,10 +4,6 @@ require 'nokogiri'
 doc = Nokogiri::HTML(open("https://news.ycombinator.com/item?id=8781768"))
 comments_h = {}
 
-# doc.css('tr').select{|a| }
-
-# doc.css('.comment')
-
 l = doc.css('table')[3].css('tr').select{|a| a.css("img").select{|l| l.attributes["width"].value == "0" }.length > 0 }.each do |comment|
   comments_h[(0...8).map { (65 + rand(26)).chr }.join] = comment.css(".comment").to_s.gsub("\"", "'")
 end
